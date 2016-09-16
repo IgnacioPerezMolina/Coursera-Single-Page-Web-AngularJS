2961083bd3a8f4e1777592a885e40f7132d26aff
Ignacio <ipmhipro@gmail.com>
2016-09-17 00:43:54 +0100

    Module 1 done

---
+++ module1-solution/app.js
@@ -0,0 +1,31 @@
+(function(){
+'use strict';
+  
+angular.module('LunchCheck', [])
+.controller('ControllerModule1', ControllerModule1);
+  
+ControllerModule1.$inject = ['$scope'];
+function ControllerModule1($scope){
+  $scope.lunch="";
+
+  $scope.checkLunch = function () {
+    var elements = $scope.lunch.split(/\s*,\w\s*/);
+    if(elements == ""){
+      $scope.message = "Please enter data first";
+      $scope.class = "alert alert-danger";
+    }
+    else if(elements.length <= 3){
+      $scope.message = "Enjoy!";
+      $scope.class = "alert alert-success";
+    }
+    else if(elements.length > 3){
+      $scope.message = "Too much!";
+      $scope.class = "alert alert-success";
+    }
+    console.log(elements.length);
+    console.log(elements);
+  }
+}
+
+})();
+
