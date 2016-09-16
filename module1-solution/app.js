7739091101741326b650876eff1960e5dab6b1f8
Ignacio <ipmhipro@gmail.com>
2016-09-17 00:52:16 +0100

    Module 1 done¡

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -12,14 +12,17 @@ function ControllerModule1($scope){
     var elements = $scope.lunch.split(/\s*,\w\s*/);
     if(elements == ""){
       $scope.message = "Please enter data first";
+      // Change class of div that contains the message
       $scope.class = "alert alert-danger";
     }
     else if(elements.length <= 3){
       $scope.message = "Enjoy!";
+      // Change class of div that contains the message
       $scope.class = "alert alert-success";
     }
     else if(elements.length > 3){
       $scope.message = "Too much!";
+      // Change class of div that contains the message 
       $scope.class = "alert alert-success";
     }
   }
